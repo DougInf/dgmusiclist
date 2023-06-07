c685ca69085d0f50ba3cd82fc6c4e01e05ac73d3
DougInf <130952647+DougInf@users.noreply.github.com>
2023-06-07 18:39:25 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+window.addEventListener('scroll', function() {
+  var scrollSection = document.getElementById('scroll-section');
+  var scrollPosition = scrollSection.getBoundingClientRect().top;
+  var screenPosition = window.innerHeight / 2;
+
+  if (scrollPosition < screenPosition) {
+    scrollSection.classList.add('show');
+  }
+  else {
+    scrollSection.classList.remove('show');
+  }
+});
+
+window.addEventListener('scroll', function() {
+  var backToTopButton = document.querySelector('.back-to-top');
+  if (window.pageYOffset > 300) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+});
+
+document.querySelector('.back-to-top').addEventListener('click', function(e) {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
